7745c1987930401d33dcc6fd358887d1a3ebcdcc
Georgi Mateev <Georgi Mateev>
2016-10-27 16:12:05 +0300

    Test commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var core = require('./core.js');
 
 const PORT=process.env.APP_PORT; 
 
-var server = http.createServer((req, res) => res.end('Result: ' + core.square(5)));
+var server = http.createServer((req, res) => res.end('Result 5*5: ' + core.square(5)));
 
 server.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
